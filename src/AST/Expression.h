d341e2356cd531935b3a8d6af1678eaa2d669014
Uladzislau <kobiakulad@gmail.com>
2023-03-08 22:48:14 +0100

    getting tokens from the input

---
+++ src/AST/Expression.h
@@ -0,0 +1,14 @@
+//
+// Created by kobia on 08.03.2023.
+//
+
+#ifndef MYLANGUAGE_EXPRESSION_H
+#define MYLANGUAGE_EXPRESSION_H
+
+
+class Expression {
+    virtual double eval() = 0;
+};
+
+
+#endif //MYLANGUAGE_EXPRESSION_H
