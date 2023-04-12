0ebb745900010e1c5adff734f2f63b88885bda5c
Uladzislau <kobiakulad@gmail.com>
2023-04-12 02:04:38 +0200

    now the program is in separate file

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include "filesystem"
 #include "Token/Token.h"
 #include "Lexer/Lexer.h"
 #include "Parser/Parser.h"
 
 using namespace std;
 
-
 int main() {
-    fstream s{R"(C:\Users\kobia\Desktop\MyLanguage\src\program.txt)"};
-    string fileContents((istreambuf_iterator<char>(s)), istreambuf_iterator<char>());
-    s.close();
-
-
+    fstream file{"../src/program.txt"}; // Exiting folder cmake-build-debug and entering the project folder
+    string fileContents((istreambuf_iterator<char>(file)),
+                        istreambuf_iterator<char>());
     vector<Token> tokens = (new Lexer(fileContents))->tokenize();
     for (auto el : tokens) {
         cout << el.getType() << " " << el.getText() << endl;
